a740afe83d74335906dbe3ef0ae850ec01d17e7d
Matej Šándor <matejko.sandor@gmail.com>
2020-05-03 19:22:43 +0200

    added class for downloading data and enum class for download status

---
+++ app/src/main/java/com/sandor/flickrbrowserapp/GetRawData.kt
@@ -0,0 +1,55 @@
+package com.sandor.flickrbrowserapp
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus() {
+    OK,IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+private const val TAG = "GetRawData"
+
+class GetRawData : AsyncTask<String, Void, String>() {
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun doInBackground(vararg params: String?): String {
+        if(params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALIZED
+            return "No URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALIZED
+                    return "MalformedURLException ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    return "IOException ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    return "Security Exception ${e.message}"
+                }
+                else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    return "Unknown Exception ${e.message}"
+                }
+            }
+            Log.e(TAG,errorMessage)
+            return errorMessage
+        }
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
+}
--- app/src/main/java/com/sandor/flickrbrowserapp/MainActivity.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sandor.flickrbrowserapp
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -9,26 +10,27 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG,"onCreate: called")
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        Log.d(TAG,"onCreate: ends here")
+        Log.d(TAG, "onCreate: ends here")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        Log.d(TAG,"onCreateOptionsMenu: called")
+        Log.d(TAG, "onCreateOptionsMenu: called")
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d(TAG,"onOptionsItemSelected: called")
+        Log.d(TAG, "onOptionsItemSelected: called")
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
