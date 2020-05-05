58168e1f1d0f6557603a75fa78588e1a4c176597
Matej Šándor <matejko.sandor@gmail.com>
2020-05-05 09:51:10 +0200

    created recycler view adapter

--- app/src/main/java/com/sandor/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
@@ -1,5 +1,44 @@
 package com.sandor.flickrbrowserapp
 
-class FlickrRecyclerViewAdapter {
-    
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "FlickrRecyclerAdapter"
+
+class FlickrImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var thumbnail = view.findViewById<ImageView>(R.id.thumbnail)
+    var title = view.findViewById<TextView>(R.id.title)
+}
+
+class FlickrRecyclerViewAdapter(private var photoList: List<Photo>) : RecyclerView.Adapter<FlickrImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickrImageViewHolder {
+        Log.d(TAG, "onCreateViewHolder: called, new view requested")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.browse,parent,false)
+        return FlickrImageViewHolder(view)
+    }
+
+    fun loadNewData(newPhotos: List<Photo>) {
+        photoList = newPhotos
+        notifyDataSetChanged()
+    }
+
+    fun getPhoto(position: Int) : Photo? {
+        return if(photoList.isNotEmpty()) photoList[position] else null
+    }
+
+    override fun getItemCount(): Int {
+        Log.d(TAG, "getItemCount: called")
+        return if (photoList.isNotEmpty()) photoList.size else 0
+    }
+
+    override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
