7730b2339e54545aa80dea57e35d43c981cc1f94
Matej Šándor <matejko.sandor@gmail.com>
2020-05-05 16:54:05 +0200

    added base activity as well as enabled toolbar

---
+++ app/src/main/java/com/sandor/flickrbrowserapp/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.sandor.flickrbrowserapp
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+private const val TAG = "BaseActivity"
+internal const val FLICKER_QUERY = "FLICKER_QUERY"
+internal const val PHOTO_TRANSFER = "PHOTO_TRANSFER"
+
+@SuppressLint("Registered")
+open class BaseActivity: AppCompatActivity() {
+
+    internal fun activateToolBar(enableHome: Boolean) {
+        Log.d(TAG, "activateToolBar: called")
+        var toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(enableHome)
+
+    }
+
+}
