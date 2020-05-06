b0bf670a46509ef9b49174648220540586d95f32
Matej Šándor <matejko.sandor@gmail.com>
2020-05-06 10:27:22 +0200

    the image is now being displayed in detail after a tap

--- app/src/main/java/com/sandor/flickrbrowserapp/PhotoDetailsActivity.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/PhotoDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.sandor.flickrbrowserapp
 
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -8,6 +10,16 @@ class PhotoDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         activateToolBar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        photo_title.text = photo.title
+        photo_author.text = photo.author
+        photo_tags.text = photo.tags
+        Picasso.get()
+            .load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
 }
