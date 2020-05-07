60afe80558163e7d7e6b0c50be422a144612ac51
Matej Šándor <matejko.sandor@gmail.com>
2020-05-07 13:12:47 +0200

    added a notification in case a search results in no matches

--- app/src/main/java/com/sandor/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/FlickrRecyclerViewAdapter.kt
@@ -35,18 +35,24 @@ class FlickrRecyclerViewAdapter(private var photoList: List<Photo>) :
     }
 
     override fun getItemCount(): Int {
-        return if (photoList.isNotEmpty()) photoList.size else 0
+        return if (photoList.isNotEmpty()) photoList.size else 1
     }
 
     override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
-        val photoItem = photoList[position]
-        Picasso.get()
-            .load(photoItem.image)
-            .error(R.drawable.placeholder)
-            .placeholder(R.drawable.placeholder)
-            .into(holder.thumbnail)
 
-        holder.title.text = photoItem.title
+        if(photoList.isEmpty()) {
+            holder.thumbnail.setImageResource(R.drawable.placeholder)
+            holder.title.setText(R.string.no_matches_notification)
+        } else {
+            val photoItem = photoList[position]
+            Picasso.get()
+                .load(photoItem.image)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail)
+
+            holder.title.text = photoItem.title
+        }
 
     }
 
