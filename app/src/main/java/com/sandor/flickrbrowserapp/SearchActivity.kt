18067af29a06583a9e672e4972b2ffa77d2c1e38
Matej Šándor <matejko.sandor@gmail.com>
2020-05-07 12:13:19 +0200

    the searchview now opens successfully

--- app/src/main/java/com/sandor/flickrbrowserapp/SearchActivity.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/SearchActivity.kt
@@ -1,6 +1,9 @@
 package com.sandor.flickrbrowserapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.widget.SearchView
 
@@ -15,8 +18,20 @@ class SearchActivity : BaseActivity() {
         activateToolBar(true)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, "onCreateOptionsMenu: called")
         menuInflater.inflate(R.menu.menu_search,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        searchView?.setSearchableInfo(searchableInfo)
+        Log.d(TAG, "onCreateOptionsMenu: $componentName")
+        Log.d(TAG, "onCreateOptionsMenu: hint is ${searchView?.queryHint}")
+        Log.d(TAG, "onCreateOptionsMenu: $searchableInfo")
+
+        searchView?.isIconified = false
+
         return true
     }
 }
