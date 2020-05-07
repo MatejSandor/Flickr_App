0aa554f4915078a07bfce4b0991d0468a55fbf78
Matej Šándor <matejko.sandor@gmail.com>
2020-05-07 11:28:04 +0200

    fixed errors

--- app/src/main/java/com/sandor/flickrbrowserapp/MainActivity.kt
+++ app/src/main/java/com/sandor/flickrbrowserapp/MainActivity.kt
@@ -67,7 +67,10 @@ class MainActivity : BaseActivity(), GetRawData.OnDownloadComplete,
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         Log.d(TAG, "onOptionsItemSelected: called")
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_search -> {
+                startActivity(Intent(this,SearchActivity::class.java))
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
